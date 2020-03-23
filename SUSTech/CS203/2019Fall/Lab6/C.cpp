3466e33adbb918f8a8643e0701f549a20cf87254
Pisces311 <909833130@qq.com>
2020-03-23 19:59:42 +0800

    update course materials for CS203 again

--- SUSTech/CS203/2019Fall/Lab6/C.cpp
+++ SUSTech/CS203/2019Fall/Lab6/C.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 typedef long long ll;
-const int maxn = 1e6 + 5;
+const int maxn = 1e4 + 5;
 
-int n, m, ans = 0;
+int N, ans = 0;
 int nxt[maxn];
 char s[maxn], p[maxn];
 
 void get_next(char *p) {
     nxt[0] = 0;
-    for (int q = 1, k = 0; q < m; ++q) {
+    int plen = strlen(p);
+    for (int q = 1, k = 0; q < plen; ++q) {
         while (k > 0 && p[q] != p[k]) k = nxt[k - 1];
         if (p[q] == p[k]) ++k;
         nxt[q] = k;
@@ -19,14 +20,12 @@ void get_next(char *p) {
 
 int KMP(char *s, char *p) {
     get_next(p);
+    int slen = strlen(s), plen = strlen(p);
     int cnt = 0;
-    for (int i = 0, j = 0; i < n; ++i) {
+    for (int i = 0, j = 0; i < slen; ++i) {
         while (j > 0 && p[j] != s[i]) j = nxt[j - 1];
-        if (p[j] == s[i]) ++j;
-        if (j == m) {
-            ++cnt;
-            j = 0;
-        }
+        if (p[j] == s[i]) cnt = max(cnt, ++j);
+        if (j == plen) j = nxt[j - 1];
     }
     return cnt;
 }
@@ -39,9 +38,12 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    cin >> n >> m;
-    cin >> s >> p;
-    cout << KMP(s, p) << '\n';
+    cin >> N;
+    while (N--) {
+        cin >> s >> p;
+        if (3 * KMP(p, s) >= (int)strlen(s)) ++ans;
+    }
+    cout << ans << '\n';
 
     return 0;
 }
